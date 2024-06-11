70520c99f17c8a3e81241940d14942f875a9a0d5
mailkaPKS <malikaabas545@gmail.com>
2024-06-11 08:12:27 +0600

    Add files via upload

---
+++ src/java_task/BEEC1006.java
@@ -0,0 +1,19 @@
+package java_task;
+
+import java.util.Scanner;
+
+public class BEEC1006 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+        double C = scanner.nextDouble();
+
+        double average = ((A * 2) + (B * 3) + (C * 5)) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", average);
+
+        scanner.close();
+    }
+}
---
+++ src/java_task/BEEC1018.java
@@ -0,0 +1,17 @@
+package java_task;
+
+import java.util.Scanner;
+
+public class BEEC1018 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(N);
+        int[] banknotes = {100, 50, 20, 10, 5, 2, 1};
+        for (int note : banknotes) {
+            int count = N / note;
+            System.out.println(count + " nota(s) de R$ " + note + ",00");
+            N %= note;
+        }
+    }
+}
